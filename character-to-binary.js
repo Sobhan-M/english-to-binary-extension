cd411fcb1ef592d74e2f097b153417323e57fba1
Sobhan-M <72103946+Sobhan-M@users.noreply.github.com>
2021-02-22 23:47:38 -0500

    Binary To Text Method Creation

--- character-to-binary.js
+++ character-to-binary.js
@@ -35,6 +35,24 @@ function stringToBinary(inputString)
     return output;
 }
 
+/**
+ * Takes a binary string as input, and converts it into human readable text.
+ * @param {string} binaryInput A space separated string of characters in binary.
+ * @returns {string} A string of human readable text.
+ */
+function binaryToString(binaryInput)
+{
+    let characters = binaryInput.split(" ");
+    let output = "";
+
+    for (let i = 0 ; i  < characters.length ; i++)
+    {
+        output += String.fromCharCode(parseInt(characters[i],2));
+    }
+
+    return output;
+}
+
 
 let inputArea = document.getElementById("input-text-js");
 let outputArea = document.getElementById("output-text-js");
@@ -42,4 +60,9 @@ let outputArea = document.getElementById("output-text-js");
 inputArea.addEventListener("input",function() {
     let text = this.value;
     outputArea.value = stringToBinary(text);
+});
+
+outputArea.addEventListener("input",function() {
+    let text = this.value;
+    inputArea.value = binaryToString(text);
 });
